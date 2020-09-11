a58ccff585227d01511e8f8c5483a78bd48a5658
Henrique <rickuev@gmail.com>
2020-09-10 22:36:56 -0300

    slpash screen

---
+++ android/MonitoraAgricultor/app/src/main/java/com/unicesumar/esoft/monitoraagricultor/DuvidaActivity.kt
@@ -0,0 +1,11 @@
+package com.unicesumar.esoft.monitoraagricultor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DuvidaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_duvida)
+    }
+}
