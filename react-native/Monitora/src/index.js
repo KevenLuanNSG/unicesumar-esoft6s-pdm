703c84c4e17f0efea0e96388205fdbc8fa8107bf
Henrique <rickuev@gmail.com>
2020-11-19 20:55:58 -0300

    app do profissional parte 1

---
+++ react-native/Monitora/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './pages/home';
+import Cadastro from './pages/cadastro';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Cadastro" component={Cadastro}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
